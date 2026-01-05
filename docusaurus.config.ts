fda31e679c6c737185f92bef36cf411a9b7bd2ab
Shyam Swaroop <30747788+cruxcode@users.noreply.github.com>
2026-01-05 10:46:18 +0800

    baseUrl is `/`
    Moved to custom domain in github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://getadriai.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/adri-docs/',
+  baseUrl: '/', // changed from `/adri-docs/` to `/` on setting custom domain in GitHub pages
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
